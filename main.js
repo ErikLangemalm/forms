6376d742abe161c2319bb522a62739bccd17a18e
Erik Langemalm <82160523+ErikLangemalm@users.noreply.github.com>
2023-10-06 17:11:33 +0200

    Update main.js

--- main.js
+++ main.js
@@ -4,51 +4,28 @@ const fs = require('fs');
 const data = fs.readFileSync('results.json');
 const jsonData = JSON.parse(data);
 
-//const questions = fs.readFileSync('questions.json');
-//const qdata = JSON.parse(questions);
-
-
-const questions = {
-  qArr: [
-    "Är du aktiv i din vardag?",
-    "Gillar du att vara ute?",
-
-  ],
-  yes: {
-    cat: [1, 1],
-    dog: [1, 2],
-    fish: [0, 0],
-    rabbit: [0, 1]
-  },
-  no: {
-    cat: [1, 1],
-    dog: [0, 0],
-    fish: [1, 1],
-    rabbit: [1, 1]
-  }
-}
+const questions = require('./questions.json');
 
 function result() {
-  this.cat = 0;
-  this.dog = 1;
-  this.fish = 0;
-  this.rabbit = 0;
+  this.katt = 0;
+  this.hund = 1;
+  this.fisk = 0;
+  this.kanin = 0;
 
 }
 
 
-function getNames(cat, dog, fish, rabbit) {
-  let res2 = new result(cat, dog, fish, rabbit);
+function getNames(katt, hund, fisk, kanin) {
+  let res2 = new result(katt, hund, fisk, kanin);
   let ans;
 
   ans = Object.keys(res2);
-
   return ans;
 }
 
 function resSaveAndPrint(nameAr, resAr) {
-  let highestIndex = 0; // Assume the highest value is at index 0 initially.
-  let highestValue = resAr[0]; // Assume the highest value is the first element.
+  let highestIndex = 0;
+  let highestValue = resAr[0];
 
   for (let i = 1; i < resAr.length; i++) {
     if (resAr[i] > highestValue) {
@@ -61,14 +38,17 @@ function resSaveAndPrint(nameAr, resAr) {
   for (let i = 0; i < nameAr.length; i++) {
     console.log(nameAr[i] + " : " + resAr[i]);
   }
-  str = "Hej jag är ett namn";
+
+  console.log("Skriv in ditt namn för att spara ditt resultat");
+  str = prompt();
 
   jsonData.results.push({
-    [str]: {
-      cat: resAr[0],
-      dog: resAr[1],
-      fish: resAr[2],
-      rabbit: resAr[3],
+    resultat: {
+      namn: str,
+      katt: resAr[0],
+      hund: resAr[1],
+      fisk: resAr[2],
+      kanin: resAr[3],
       date: Date()
     }
   })
@@ -77,13 +57,13 @@ function resSaveAndPrint(nameAr, resAr) {
 }
 
 
-function compare(cat, dog, fish, rabbit) {
+function compare(katt, hund, fisk, kanin) {
 
   console.clear();
   resNames = [];
-  res = [cat, dog, fish, rabbit];
+  res = [katt, hund, fisk, kanin];
   let sum = 0;
-  resNames = getNames(cat, dog, fish, rabbit);
+  resNames = getNames(katt, hund, fisk, kanin);
   for (let i = 0; i < res.length; i++) {
     sum += parseInt(res[i]);
   }
@@ -98,47 +78,39 @@ function compare(cat, dog, fish, rabbit) {
   resSaveAndPrint(resNames, res)
 
 }
+function main() {
+  console.log("Hi and welcome to this quiz!");
+  console.log("Every question is answered with yes or no. Wrong answer or spelling restarts the quiz");
+  let yourResult = new result();
+  for (let i = 0; i < questions.length; i++) {
+    console.log(questions[i].q);
+    answer = toString(prompt().toLowerCase());
 
-function form(cat, dog, fish, rabbit) {
-  for (let i = 0; i < questions.qArr.length; i++) {
-    console.log(questions.qArr[i]);
-    answer = prompt().toLowerCase();
-    if (answer != "yes" && answer != "no") {
+    if (answer = "ja") {
+      yourResult.katt += questions[i].ja.katt;
+      yourResult.hund += questions[i].ja.hund;
+      yourResult.fisk += questions[i].ja.fisk;
+      yourResult.kanin += questions[i].ja.kanin;
+      console.clear();
+    }
+    else if (answer = "nej") {
+      yourResult.katt += questions[i].nej.katt;
+      yourResult.hund += questions[i].nej.hund;
+      yourResult.fisk += questions[i].nej.fisk;
+      yourResult.kanin += questions[i].nej.kanin;
+      console.clear();
+    }
+    else {
       console.log("Du har matat in ett felaktigt svar");
-      //console.clear();
-      form(cat, dog, fish, rabbit);
+      console.clear();
+      i--;
+
     }
-    switch (answer) {
-      case "yes":
-        cat += parseInt(questions.yes.cat[i]);
-        dog += parseInt(questions.yes.dog[i]);
-        fish += parseInt(questions.yes.fish[i]);
-        rabbit += parseInt(questions.yes.rabbit[i]);
-        console.clear();
-      case "no":
-        cat += questions.no.cat[i];
-        dog += questions.no.dog[i];
-        fish += questions.no.fish[i];
-        rabbit += questions.no.rabbit[i];
-        console.clear();
-      default:
-        break;
-    }
+
   }
-  compare(cat, dog, fish, rabbit);
+  compare(yourResult.katt, yourResult.hund, yourResult.fisk, yourResult.kanin);
 }
 
-
-console.log("Hi and welcome to this quiz!");
-console.log("Every question is answered with yes or no. Wrong answer or spelling restarts the quiz");
-let yourResult = new result();
-
-answer = form(yourResult.cat, yourResult.dog, yourResult.fish, yourResult.rabbit);
-//compare(yourResult);
-//console.log(jsonData.results);
-
-
-//fs.writeFileSync('results.json', JSON.stringify(jsonData));
-
+main()
 
 
